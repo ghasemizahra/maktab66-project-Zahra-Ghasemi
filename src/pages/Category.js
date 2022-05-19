98ca8b97314e99ca03ce2da4da882e0134198bd8
ghasemizahra <z.ghasemi.02018@gmail.com>
2022-05-19 18:18:48 +0430

    add

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,37 +1,14 @@
 import React from 'react'
 import User from '../Layouts/User'
 import Sidebar from '../Components/Sidebar'
-import { Link, useLocation, Navigate } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
-import { Box } from '@mui/system';
-import { Cardstyle } from '../Assest/Style/Stylecomponent';
-import Cards from '../Components/Home/Cards'
 
-function Category() {
-  const product = useSelector((state) => state.product)
-  const location = useLocation()
-  const { from } = location.state
+ function Category() {
   return (
     <div>
-      {product.product == null ? <Navigate to='/category' /> :
-        <>
-          <h1>{from.name}</h1>
-          {product.product.map((item, i) => {
-            if (item.category == from.id) {
-              return (
-                  <Cards item={item} key={i} />
-              )
-            }
-
-          })}
-        </>
-      }
+        <p>main</p>
+        <div><Sidebar/></div>
     </div>
-
+    
   )
 }
 export default User(Category)
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -50,7 +50,7 @@ function Home() {
       {category == null ? <Navigate to='/' /> :
         category.map((item) => (
           <>
-            <Linkstyle to='/category'   state={{ from: item }}>
+            <Linkstyle to='/category'>
               <h2> {item.name}</h2>
             </Linkstyle>
             <Main idCategory={item.id} key={item.id} /></>
@@ -59,5 +59,3 @@ function Home() {
     </Box>
   )
 } export default User(Home)
-
-
