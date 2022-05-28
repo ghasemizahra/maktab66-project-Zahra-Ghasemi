bf328a3e144abccf98ea6105c815ea4a5389a206
ghasemizahra <z.ghasemi.02018@gmail.com>
2022-05-28 14:27:33 +0430

    add

--- src/hooks/WithLayoutPages.jsx
+++ src/hooks/WithLayoutPages.jsx
@@ -1,21 +1,21 @@
-import React from "react";
-import User from "../Layouts/User";
-import Admin from "../Layouts/Admin";
+// import React from "react";
+// import User from "../Layouts/User";
+// import Admin from "../Layouts/Admin";
 
-const WithLayoutpages = (Component, role = 'user') => {
-  return (props) => {
-    if (role == 'user')
-      return (
-        <User>
-          <Component {...props} />
-        </User>
-      );
-    return (
-      <Admin>
-        <Component {...props} />
-      </Admin>
-    );
-  };
-};
+// const WithLayoutpages = (Component, role = 'user') => {
+//   return (props) => {
+//     if (role == 'user')
+//       return (
+//         <User>
+//           <Component {...props} />
+//         </User>
+//       );
+//     return (
+//       <Admin>
+//         <Component {...props} />
+//       </Admin>
+//     );
+//   };
+// };
 
-export default WithLayoutpages;
+// export default WithLayoutpages;
