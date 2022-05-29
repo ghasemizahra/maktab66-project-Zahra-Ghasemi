77952b8739bbe4cc234c6948247fb1b6d6b40d7b
ghasemizahra <z.ghasemi.02018@gmail.com>
2022-05-29 13:30:11 +0430

    add

--- src/Components/Stack/Stack.js
+++ src/Components/Stack/Stack.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Grid,Button } from "@mui/material";
-import TableStack from "../../pages/Inventory";
+import TableStack from "../Stack/TableStack";
 import axios from "axios";
-
+import Admin from "../../Layouts/Admin"
 function Stack() {
   const [row, setRow] = useState([]);
   const[flag,setFlag]=useState({"id":true})
@@ -33,4 +33,4 @@ function Stack() {
     </Grid>
   );
 }
-export { Stack };
+export default Admin(Stack) ;
--- src/n/n.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function n() {
-  return (
-    <div>n</div>
-  )
-}
