ad4bf32f7daaa599b108e4ad64376b61ff8fbed7
ghasemizahra <z.ghasemi.02018@gmail.com>
2022-06-07 20:00:32 +0430

    ptoduucts

---
+++ src/pages/OutletOrder.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
+import Admin from '../Layouts/Admin'
+
+
+function OutletOrder() {
+    return (
+        <>
+
+           <span>سفارش های در حال انتظار ارسال</span>  
+
+
+           
+            <Link to={`/paneladmin/orders`} ><input type="radio"  name="orderRadio" value="waiting"  /></Link>
+            <Link to={`/paneladmin/orders/resived`} ><input type="radio" name="orderRadio" value="resive" /></Link>
+            <span>سفارش های تحویل داده شده</span>
+           
+            
+    <Outlet/>
+
+        </>
+
+    )
+}
+export default Admin(OutletOrder)
