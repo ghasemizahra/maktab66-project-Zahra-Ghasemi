87defa4161ca0376e097af2494a141425a259394
ghasemizahra <z.ghasemi.02018@gmail.com>
2022-06-10 09:47:28 +0430

    add

--- src/Components/Stack/Stack.js
+++ src/Components/Stack/Stack.js
@@ -1,229 +1,229 @@
 
-import Admin from "../Layouts/Admin";
+// import Admin from "../Layouts/Admin";
 
-import * as React from "react";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import { useEffect, useState } from "react";
-import {api} from "../services/Config";
-import Pagination from "@mui/material/Pagination";
-import { Box } from "@mui/material";
-import PriceEditmode from "../Components/PriceStock/priceEditmode";
-import CountEditmode from "../Components/PriceStock/countEditmode";
-import { CircularProgress } from "@mui/material";
-function Stack() {
-  const [data, setData] = useState({});
-  const [isLoading, setLoading] = useState(true);
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
-  const [editdata, seteditdata] = useState();
-  const [editId, seteditId] = useState()
-  const handleChange = (event, value) => {
-    setPage(value);
-  };
-  function productdata(page, items) {
-    api
-      .get(`/products`, {params:{
-        _page:`${page}`,
-        _limit:`${items}`,
-        _sort:"createdAt",
-        _order:"desc",
-        // orderStatus:`${status}`
-      }})
-      .then((res) => {
-        // console.log(res.data);
-        setData(res.data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-  let  [arr,setArr] = useState([])
+// import * as React from "react";
+// import Table from "@mui/material/Table";
+// import TableBody from "@mui/material/TableBody";
+// import TableCell from "@mui/material/TableCell";
+// import TableContainer from "@mui/material/TableContainer";
+// import TableHead from "@mui/material/TableHead";
+// import TableRow from "@mui/material/TableRow";
+// import Paper from "@mui/material/Paper";
+// import { useEffect, useState } from "react";
+// import {api} from "../../sevices/Config";
+// import Pagination from "@mui/material/Pagination";
+// import { Box } from "@mui/material";
+// import PriceEditmode from "./priceEditmode";
+// import CountEditmode from "./countEditmode";
+// import { CircularProgress } from "@mui/material";
+// function Stack() {
+//   const [data, setData] = useState({});
+//   const [isLoading, setLoading] = useState(true);
+//   const [page, setPage] = React.useState(0);
+//   const [rowsPerPage, setRowsPerPage] = React.useState(5);
+//   const [editdata, seteditdata] = useState();
+//   const [editId, seteditId] = useState()
+//   const handleChange = (event, value) => {
+//     setPage(value);
+//   };
+//   function productdata(page, items) {
+//     api
+//       .get(`/products`, {params:{
+//         _page:`${page}`,
+//         _limit:`${items}`,
+//         _sort:"createdAt",
+//         _order:"desc",
+//         // orderStatus:`${status}`
+//       }})
+//       .then((res) => {
+//         // console.log(res.data);
+//         setData(res.data);
+//         setLoading(false);
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   }
+//   let  [arr,setArr] = useState([])
 
-  let  [kala,setKala] = useState ([])
-  const container = {};
-  function editCount(id,data) {
-    let item = { data }
-    // setKala({...kala,data})
-    // kala={...kala,data}
-    container['name'] = data.name;
-    container['category'] = data.category;
-    container['price'] = data.price;
-    container['count'] = data.price;
-    container['description'] = data.description;
-    container['images'] = data.images;
-    container['image'] = data.image;
-    container['createdAt'] = data.createdAt;
-    container['id'] = data.id;
-    return container
-  }
-  function editTask(id,data) {
-    let item = { data }
-    // kala={...kala,data}
-    container['name'] = data.name;
-    container['category'] = data.category;
-    container['price'] = data.price;
-    container['count'] = data.price;
-    container['description'] = data.description;
-    container['images'] = data.images;
-    container['image'] = data.image;
-    container['createdAt'] = data.createdAt;
-    container['id'] = data.id;
+//   let  [kala,setKala] = useState ([])
+//   const container = {};
+//   function editCount(id,data) {
+//     let item = { data }
+//     // setKala({...kala,data})
+//     // kala={...kala,data}
+//     container['name'] = data.name;
+//     container['category'] = data.category;
+//     container['price'] = data.price;
+//     container['count'] = data.price;
+//     container['description'] = data.description;
+//     container['images'] = data.images;
+//     container['image'] = data.image;
+//     container['createdAt'] = data.createdAt;
+//     container['id'] = data.id;
+//     return container
+//   }
+//   function editTask(id,data) {
+//     let item = { data }
+//     // kala={...kala,data}
+//     container['name'] = data.name;
+//     container['category'] = data.category;
+//     container['price'] = data.price;
+//     container['count'] = data.price;
+//     container['description'] = data.description;
+//     container['images'] = data.images;
+//     container['image'] = data.image;
+//     container['createdAt'] = data.createdAt;
+//     container['id'] = data.id;
 
-    return container
+//     return container
 
 
-    // setKala({...kala,data})
+//     // setKala({...kala,data})
 
-    // api
-    //   .put(`/products/${id}`, data)
-    //   .then((res) => {
-      //     console.log(res);
-      //     // setstockeditmode(false);
-      //   })
-      //   .catch((err) => {
-        //     console.log(err);
-        //   });
-      }
-console.log(kala)
-  const changeprice = (input) => {
-    seteditdata({
-      price: input,
-    });
-  };
-  const changecount = (input) => {
-    seteditdata({
-      count: input,
-    });
-  };
-  useEffect(() => {
-    productdata(page, rowsPerPage);
-  }, [page, rowsPerPage]);
+//     // api
+//     //   .put(`/products/${id}`, data)
+//     //   .then((res) => {
+//       //     console.log(res);
+//       //     // setstockeditmode(false);
+//       //   })
+//       //   .catch((err) => {
+//         //     console.log(err);
+//         //   });
+//       }
+// console.log(kala)
+//   const changeprice = (input) => {
+//     seteditdata({
+//       price: input,
+//     });
+//   };
+//   const changecount = (input) => {
+//     seteditdata({
+//       count: input,
+//     });
+//   };
+//   useEffect(() => {
+//     productdata(page, rowsPerPage);
+//   }, [page, rowsPerPage]);
 
-  if (isLoading) {
-    return <CircularProgress />
+//   if (isLoading) {
+//     return <CircularProgress />
 
-  }
-  function save(){
-    console.log("data",data)
-    if (kala == null){
-      console.log("null")
-    }else{
-      // console.log(kala.data)
-      const id = container.id
-      // container['name'] = data.name;
-      // container['category'] = data.category;
-      // container['description'] = data.description;
-      // container['images'] = data.images;
-      // container['image'] = data.image;
-      // container['createdAt'] = data.createdAt;
+//   }
+//   function save(){
+//     console.log("data",data)
+//     if (kala == null){
+//       console.log("null")
+//     }else{
+//       // console.log(kala.data)
+//       const id = container.id
+//       // container['name'] = data.name;
+//       // container['category'] = data.category;
+//       // container['description'] = data.description;
+//       // container['images'] = data.images;
+//       // container['image'] = data.image;
+//       // container['createdAt'] = data.createdAt;
 
-      api
-      .put(`/products/${id}`, container)
-      .then((res) => {
-          console.log(res);
-          // setstockeditmode(false);
-        })
-        .catch((err) => {
-            console.log(err);
-          });
-    }
-  }
-  return (
-    <>
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <h1>موجودی و قیمت ها</h1>
-        <button
-          style={{ height: "50px", margin: "20px" }}
-          onClick={save}
-        >
-          ذخیره
-        </button>
-      </div>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell align="right">نام</TableCell>
-              <TableCell align="right">قیمت</TableCell>
-              <TableCell align="right">موجودی</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {data.map((row) => (
-              <TableRow
-                key={row.name}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell component="th" scope="row" align="right">
-                  {row.name}
-                </TableCell>
-                <TableCell component="th" scope="row" align="right">
-                  <PriceEditmode
-                    value={row.price}
-                    data={
-                      {
-                        id:row.id,
-                        image: row.image,
-                        name: row.name,
-                        category: row.category,
-                        price: row.price,
-                        // count: row.count,
-                        description: row.description,
-                        images: row.images,
-                        image: row.image,
-                        createdAt: row.createdAt
+//       api
+//       .put(`/products/${id}`, container)
+//       .then((res) => {
+//           console.log(res);
+//           // setstockeditmode(false);
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//           });
+//     }
+//   }
+//   return (
+//     <>
+//       <div style={{ display: "flex", justifyContent: "space-between" }}>
+//         <h1>موجودی و قیمت ها</h1>
+//         <button
+//           style={{ height: "50px", margin: "20px" }}
+//           onClick={save}
+//         >
+//           ذخیره
+//         </button>
+//       </div>
+//       <TableContainer component={Paper}>
+//         <Table sx={{ minWidth: 650 }} aria-label="simple table">
+//           <TableHead>
+//             <TableRow>
+//               <TableCell align="right">نام</TableCell>
+//               <TableCell align="right">قیمت</TableCell>
+//               <TableCell align="right">موجودی</TableCell>
+//             </TableRow>
+//           </TableHead>
+//           <TableBody>
+//             {data.map((row) => (
+//               <TableRow
+//                 key={row.name}
+//                 sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+//               >
+//                 <TableCell component="th" scope="row" align="right">
+//                   {row.name}
+//                 </TableCell>
+//                 <TableCell component="th" scope="row" align="right">
+//                   <PriceEditmode
+//                     value={row.price}
+//                     data={
+//                       {
+//                         id:row.id,
+//                         image: row.image,
+//                         name: row.name,
+//                         category: row.category,
+//                         price: row.price,
+//                         // count: row.count,
+//                         description: row.description,
+//                         images: row.images,
+//                         image: row.image,
+//                         createdAt: row.createdAt
 
-                      }}
-                    changeprice={editTask}
-                    id={row.id}
-                  />
-                </TableCell>
-                <TableCell component="th" scope="row" align="right">
-                  <CountEditmode
-                    value={row.count}
-                    data={
-                      {
-                        id:row.id,
+//                       }}
+//                     changeprice={editTask}
+//                     id={row.id}
+//                   />
+//                 </TableCell>
+//                 <TableCell component="th" scope="row" align="right">
+//                   <CountEditmode
+//                     value={row.count}
+//                     data={
+//                       {
+//                         id:row.id,
 
-                        image: row.image,
-                        name: row.name,
-                        category: row.category,
-                        // price: row.price,
-                        count: row.count,
-                        description: row.description,
-                        createdAt: row.createdAt
+//                         image: row.image,
+//                         name: row.name,
+//                         category: row.category,
+//                         // price: row.price,
+//                         count: row.count,
+//                         description: row.description,
+//                         createdAt: row.createdAt
                         
-                        // subcategory: row.subcategory,
-                      }
-                    }
+//                         // subcategory: row.subcategory,
+//                       }
+//                     }
                     
-                    changecount={editCount}
-                    id={row.id}
-                  />
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <Box>
-        <Pagination
-          count={10}
-          page={page}
-          dir="ltr"
-          defaultPage={1}
-          onChange={handleChange}
-        />
-      </Box>
-    </>
-  );
-}
+//                     changecount={editCount}
+//                     id={row.id}
+//                   />
+//                 </TableCell>
+//               </TableRow>
+//             ))}
+//           </TableBody>
+//         </Table>
+//       </TableContainer>
+//       <Box>
+//         <Pagination
+//           count={10}
+//           page={page}
+//           dir="ltr"
+//           defaultPage={1}
+//           onChange={handleChange}
+//         />
+//       </Box>
+//     </>
+//   );
+// }
 
-export default Admin(Stack);
+// export default Admin(Stack);
--- src/Components/WaitingOrder.js
+++ src/Components/WaitingOrder.js
@@ -1,72 +1,72 @@
-import React, { useState, useEffect } from "react";
-import Admin from "../Layouts/Admin"
-import {
-  Box,
-  Grid,
-  FormControl,
-  Radio,
-  RadioGroup,
-  FormControlLabel,
-  FormLabel,
-} from "@mui/material";
-import TableOrders from "./Stack/TableOrders";
-import axios from "axios";
+// import React, { useState, useEffect } from "react";
+// import Admin from "../Layouts/Admin"
+// import {
+//   Box,
+//   Grid,
+//   FormControl,
+//   Radio,
+//   RadioGroup,
+//   FormControlLabel,
+//   FormLabel,
+// } from "@mui/material";
+// import TableOrders from "./Stack/TableOrders";
+// import axios from "axios";
 
-function WaitingOrder() {
-  const [row, setRow] = useState([]);
+// function WaitingOrder() {
+//   const [row, setRow] = useState([]);
 
-  const [selectedValue, setSelectedValue] = React.useState("1");
+//   const [selectedValue, setSelectedValue] = React.useState("1");
 
-  const handleChange = (event) => {
-    setSelectedValue(event.target.value);
-  };
+//   const handleChange = (event) => {
+//     setSelectedValue(event.target.value);
+//   };
 
-  // const getorders = async () => {
-  //   const res = await fetch(
-  //     `http://localhost:3002/orders?orderStatus=${selectedValue}`
-  //   );
-  //   const data = await res.json();
-  //   setRow(data);
-  // };
-  useEffect(() => {
-    axios
-      .get(`http://localhost:3002/orders?orderStatus=${selectedValue}`)
-      .then((res) => setRow(res.data));
-  }, [selectedValue]);
+//   // const getorders = async () => {
+//   //   const res = await fetch(
+//   //     `http://localhost:3002/orders?orderStatus=${selectedValue}`
+//   //   );
+//   //   const data = await res.json();
+//   //   setRow(data);
+//   // };
+//   useEffect(() => {
+//     axios
+//       .get(`http://localhost:3002/orders?orderStatus=${selectedValue}`)
+//       .then((res) => setRow(res.data));
+//   }, [selectedValue]);
 
-  console.log(row);
+//   console.log(row);
 
-  return (
-    <Grid container  >
-      <Grid item xs={12} md={8}>
-        <FormControl>
-          <RadioGroup
-            row
-            aria-labelledby="demo-row-radio-buttons-group-label"
-            name="row-radio-buttons-group"
-            sx={{ mr:30, mt:5 }}
-          >
-            <FormControlLabel
-              onChange={handleChange}
-              value="1"
-              control={<Radio />}
-              label="سفارشات تحویل داده شده"
-              sx={{mr:10}}
-            />
-            <FormControlLabel
-              onChange={handleChange}
-              value="2"
-              control={<Radio />}
-              label="سفارشات در حال تحویل "
-              sx={{mr:10}}
-            />
-          </RadioGroup>
-        </FormControl>
-      </Grid>
-      <Grid item xs={12} md={8}>
-        <TableOrders row={row} />
-      </Grid>
-    </Grid>
-  );
-}
-export default Admin(WaitingOrder) ;
+//   return (
+//     <Grid container  >
+//       <Grid item xs={12} md={8}>
+//         <FormControl>
+//           <RadioGroup
+//             row
+//             aria-labelledby="demo-row-radio-buttons-group-label"
+//             name="row-radio-buttons-group"
+//             sx={{ mr:30, mt:5 }}
+//           >
+//             <FormControlLabel
+//               onChange={handleChange}
+//               value="1"
+//               control={<Radio />}
+//               label="سفارشات تحویل داده شده"
+//               sx={{mr:10}}
+//             />
+//             <FormControlLabel
+//               onChange={handleChange}
+//               value="2"
+//               control={<Radio />}
+//               label="سفارشات در حال تحویل "
+//               sx={{mr:10}}
+//             />
+//           </RadioGroup>
+//         </FormControl>
+//       </Grid>
+//       <Grid item xs={12} md={8}>
+//         <TableOrders row={row} />
+//       </Grid>
+//     </Grid>
+//   );
+// }
+// export default Admin(WaitingOrder) ;
